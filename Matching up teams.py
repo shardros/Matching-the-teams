3420bfdeaabcef93cfb0ab2d2e0104fe3c9c8619
Edwin Shepherd <shardro.s@gmail.com>
2018-03-28 13:41:48 +0100

    Add files via upload

--- Matching up teams.py
+++ Matching up teams.py
@@ -7,27 +7,37 @@ teamQueue = deque(range(1, numberOfTeams))
 out = []
 
 for i in range (0, numberOfTeams-1):
-    """
-    For the number of rounds we have.
-    A round is a set of games where each team plays once only.
-    The teams are then roated through a queue at the end of each round
-    This then seeds the unique start for next round.
-    """
 
     for x in range (1,numberOfTeams//2):
-        #Goes through the queue paring teams up from either end
-        out.append((teamQueue[x], teamQueue[numberOfTeams - x - 1]))
+        out.append([teamQueue[x], teamQueue[numberOfTeams - x - 1]])
 
-    #One team needs to stay 'stationary' in the queue
-    out.insert((i*((numberOfTeams//2) + 1)), (teamQueue[0] , numberOfTeams))
-
-    
-    #Move everyone along and wrap the end    
+    out.insert(((i*(numberOfTeams//2) + 1)), [teamQueue[0] , numberOfTeams])
+  
     teamQueue.append(teamQueue[0])
     teamQueue.popleft()
 
-    
 
-for i in range (0, len(out)):
-    print(out[i][0], " Vs ", out[i][1])
+length = len(out)
 
+out.append(['No team','No team'])    
+out.append(['No team','No team'])
+
+rounds = []
+
+for i in range (0, length, 2):
+    rounds.append((out[i][0], ",", out[i][1], "," ,out[i+1][0], ",", out[i+1][1]))
+    print(out[i][0], ",", out[i][1], "," ,out[i+1][0], ",", out[i+1][1])
+
+    if out[i][0] in [out[i-2][0],out[i-2][1],out[i-1][0],out[i-1][1]]:
+        print("WARNING FOR TEAM: ", out[i][0])
+        
+    if out[i][1] in [out[i-2][0],out[i-2][1],out[i-1][0],out[i-1][1]]:
+        print("WARNING FOR TEAM: ", out[i][1])
+        
+    if out[i+1][0] in [out[i-2][0],out[i-2][1],out[i-1][0],out[i-1][1]]:
+        print("WARNING FOR TEAM: ", out[i+1][0])
+        
+    if out[i+1][1] in [out[i-2][0],out[i-2][1],out[i-1][0],out[i-1][1]]:
+        print("WARNING FOR TEAM: ", out[i+1][1])
+
+print(rounds)    
